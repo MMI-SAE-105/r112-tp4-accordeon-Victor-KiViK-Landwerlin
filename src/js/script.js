dd611e4097b6a1996e3e85f885bacb55dcacc951
Victor-KiViK-Landwerlin <150051582+Victor-KiViK-Landwerlin@users.noreply.github.com>
2024-11-27 14:39:01 +0100

    refermer les details en cliquant sur un summary

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
-// *** ACCORDEON ***
+// *** ACCORDEON 
+const accordeon = document.querySelector(".accordeon");
+
+accordeon.addEventListener("click", (evt) => {
+    if (evt.target.matches("summary, summary *")) {
+    accordeon.querySelectorAll("details").forEach((det) => (det.open = false));
+  }}
+);
